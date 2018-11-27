0993032ff228248b68e9a891bb89b3ff1e85d20c
Aliaksandr <aleksanteri.s111@gmail.com>
2018-11-27 15:47:41 +0300

    №1 13.10

---
+++ Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Test\XmlFiles\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__('Menu Test Xml Files'));
+        $resultPage->addBreadcrumb(__('Test Xml'), __('Test Xml'));
+        return $resultPage;
+    }
+
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Test_XmlFiles::rule3');
+    }
+}
